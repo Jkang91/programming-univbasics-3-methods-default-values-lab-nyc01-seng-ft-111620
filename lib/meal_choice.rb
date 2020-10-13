4b8ffd34d0a5d64e4d1e255f471c9fcd5258b084
June Young Kang <jkang34@gmail.com>
2020-10-13 06:43:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,5 @@ def meal_choice(protein = "tofu", veg1, veg2)
   puts "What a nutritious meal!"
   food = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts food
-  return protein
-  food
+  return food
 end
